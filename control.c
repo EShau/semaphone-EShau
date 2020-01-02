0f305322af34937c33a89c2847579a072448cf6f
EShau <EShau00@stuy.edu>
2020-01-02 16:58:29 -0500

    Done

--- control.c
+++ control.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 #define KEY 69420
-#define SEG_SIZE 200
+#define LAST_LINE 4
 #define STORY_SIZE 500
 
 union semun {
@@ -23,31 +23,26 @@ union semun {
 
 int main(int argc, char *argv[]){
   if (argc == 2){
-    int semd;
-    int shmd;
-    int fd;
-    int v;
-    int r;
-    int rm;
+    int semd, shmd, fd, v, r, rm;
     if (strcmp(argv[1], "-c") == 0){
       semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
       if (semd == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error semd %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
       union semun us;
       us.val = 1;
       semctl(semd, 0, SETVAL, us);
       printf("semaphore created\n");
-      shmd = shmget(KEY, SEG_SIZE, IPC_CREAT | IPC_EXCL | 0644);
+      shmd = shmget(KEY, LAST_LINE, IPC_CREAT | IPC_EXCL | 0644);
       if (shmd == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error shmd %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
       printf("shared memory created\n");
       fd = open("semaphone.txt", O_CREAT, 0644);
       if (fd == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error fd %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
       printf("file created\n");
@@ -55,36 +50,65 @@ int main(int argc, char *argv[]){
     }
     if (strcmp(argv[1], "-v") == 0){
       char buffer[STORY_SIZE];
+      buffer[0] = 0;
       printf("The story thus far\n");
       fd = open("semaphone.txt", O_RDONLY);
       if (fd == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error fd %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
-      r = read(fd, buffer, sizeof(buffer));
+      r = read(fd, buffer, STORY_SIZE);
       if (r == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error r %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
+      buffer[r] = 0;
       printf("%s\n", buffer);
       close(fd);
     }
     if (strcmp(argv[1], "-r") == 0){
+      printf("trying to get in\n");
+      char buffer[STORY_SIZE];
+      buffer[0] = 0;
+      printf("The story thus far\n");
+      fd = open("semaphone.txt", O_RDONLY);
+      if (fd == -1){
+        printf("error fd %d: %s\n", errno, strerror(errno));
+        //exit(1);
+      }
+      r = read(fd, buffer, STORY_SIZE);
+      if (r == -1){
+        printf("error r %d: %s\n", errno, strerror(errno));
+        //exit(1);
+      }
+      buffer[r] = 0;
+      printf("%s\n", buffer);
+      close(fd);
       rm = remove("semaphone.txt");
       if (rm == -1){
-         printf("error %d: %s\n", errno, strerror(errno));
+         printf("error rm %d: %s\n", errno, strerror(errno));
          //exit(1);
       }
       printf("file removed\n");
-      shmctl(shmd, IPC_RMID, 0);
+      shmd = shmget(KEY, LAST_LINE, 0);
+      if (shmd == -1){
+        printf("error shmd %d: %s\n", errno, strerror(errno));
+        //exit(1);
+      }
+      rm = shmctl(shmd, IPC_RMID, 0);
       if (rm == -1){
-        printf("error %d: %s\n", errno, strerror(errno));
+        printf("error rm %d: %s\n", errno, strerror(errno));
         //exit(1);
       }
       printf("shared memory removed\n");
-      semctl(semd, IPC_RMID, 0);
+      semd = semget(KEY, 1, 0);
+      if (semd == -1){
+        printf("error semd %d: %s\n", errno, strerror(errno));
+        //exit(1);
+      }
+      rm = semctl(semd, IPC_RMID, 0);
       if (rm == -1){
-         printf("error %d: %s\n", errno, strerror(errno));
+         printf("error rm %d: %s\n", errno, strerror(errno));
          //exit(1);
       }
       printf("semaphore removed\n");
--- write.c
+++ write.c
@@ -10,19 +10,14 @@
 #include <unistd.h>
 
 #define KEY 69420
-#define LAST_LINE 200
+#define LAST_LINE 4
 
 int main(){
   printf("trying to get in\n");
-  int semd;
   struct sembuf sb;
-  int op;
-  int shmd;
-  char * data;
-  int fd;
-  int w;
-  int shmt;
-  int c;
+  int * size;
+  char data[256];
+  int semd, op, shmd, fd, w, shmt, c;
   semd = semget(KEY, 1, 0);
   if (semd == -1){
     printf("error %d: %s\n", errno, strerror(errno));
@@ -33,46 +28,49 @@ int main(){
   sb.sem_op = -1;
   op = semop(semd, &sb, 1);
   if (op == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error op %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
   shmd = shmget(KEY, LAST_LINE, 0);
   if (shmd == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error shmd %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
-  sb.sem_num = 0;
-  sb.sem_flg = 0;
-  sb.sem_op = -1;
-  op = semop(semd, &sb, 1);
-  data = shmat(shmd, 0, 0);
-  fd = open("semaphone.txt", O_APPEND);
+  size = shmat(shmd, 0, 0);
+  fd = open("semaphone.txt", O_RDWR);
   if (fd == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error fd %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
-  printf("Last addition: %s\n", data);
+  char buffer[256];
+  printf("%d\n", * size);
+  lseek(fd, -*size, SEEK_END);
+  read(fd, buffer, *size);
+  buffer[*size] = 0;
+  printf("Last addition: %s\n", buffer);
   printf("Your addition: ");
-  fgets(data, LAST_LINE, stdin);
+  fgets(data, 256, stdin);
+  data[strlen(data)] = 0;
+  * size = strlen(data);
   w = write(fd, data, strlen(data));
   if (w == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error w %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
-  shmt = shmdt(data);
+  shmt = shmdt(size);
   if (shmt == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error shmt %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
   c = close(fd);
   if (c == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error c %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
   sb.sem_op = 1;
   op = semop(semd, &sb, 1);
   if (op == -1){
-    printf("error %d: %s\n", errno, strerror(errno));
+    printf("error op %d: %s\n", errno, strerror(errno));
     //exit(1);
   }
 }
